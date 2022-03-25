63b0cfd605c057957654fdfe6c267e04005ece54
jhy950210 <66720964+jhy950210@users.noreply.github.com>
2022-03-25 16:48:33 +0900

    feat(baseball): 게임 진행 class

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,32 @@
+package baseball;
+
+public class Game {
+    private int strike = 0;
+    private int ball = 0;
+
+    public Game(Balls balls, String input) {
+        for (int i = 0; i < 3; i++) {
+            int nowBallNo = Integer.parseInt(input.substring(i, i + 1));
+
+            countStrikeOrBall(balls.compare(new Ball(i + 1, nowBallNo)));
+        }
+    }
+
+    private void countStrikeOrBall(BallType ballType) {
+        if(ballType.equals(BallType.STRIKE)){
+            strike++;
+        }
+
+        if(ballType.equals(BallType.BALL)){
+            ball++;
+        }
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
